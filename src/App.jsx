44ba1d308355f07dde1685fa47d9ced7adb7ebcd
Marko Matic <maticmarko08@gmail.comgit config --global user.email ,maticmarko08@gmail.com>
2025-04-06 20:31:40 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import { useState } from 'react'
 import './App.css'
 
 let hoursDate=new Date().getHours()
-let modes=hoursDate<18?"blue":"red"
+let modes=hoursDate>7&&hoursDate<20?"yellow":"magenta"
 
 function App() {
  
   return (
     <>
 
-   <div style={{color:modes}}>
-Sada je tacno {hoursDate} sati i zato je boja slova {modes}
+   <div style={{background:modes}}>
+Sada je  {hoursDate} sati i naki minut i zato je boja pozadine {modes}
    </div>
      
       
