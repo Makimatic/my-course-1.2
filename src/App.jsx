0ea83a4bb0efc5313fe0f1aae9bf8d39b0706da3
Marko Matic <maticmarko08@gmail.comgit config --global user.email ,maticmarko08@gmail.com>
2025-05-13 15:33:36 +0200

    Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
+import Products from "./Components/Products";
+
+import "./App.css";
 
 
-import { useState } from 'react'
-
-import './App.css'
-
-let hoursDate=new Date().getHours()
-let modes=hoursDate>7&&hoursDate<20?"yellow":"magenta"
 
 function App() {
- 
   return (
     <>
-
-   <div style={{background:modes}}>
-Sada je  {hoursDate} sati i naki minut i zato je boja pozadine {modes}
-   </div>
-     
       
-      </>
-  )
+      <Products tax="20" />
+      <Products tax="33" />
+      <Products tax="50" />
+    </>
+  );
 }
 
-export default App
+export default App;
